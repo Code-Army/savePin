62c950753981d932d54e1b3c8147e7702a245c9f
Rathnakumara.G.G.K.M <kusalrathnakumara@gmail.com>
2021-06-13 13:14:42 +0530

    pin crud added

---
+++ flutter_with_db_sample/lib/pages/UpdatePin.dart
@@ -0,0 +1,132 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cool_alert/cool_alert.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_with_db_sample/models/pin.dart';
+import 'package:flutter_with_db_sample/widgets/edit_pin_form.dart';
+
+
+class EditScreen extends StatefulWidget{
+  final String CurrentName;
+  final String CurrentPin;
+  final String CurrentUsername;
+  final String CurrentType;
+  final String documentId;
+
+  EditScreen({
+     this.CurrentName,
+     this.CurrentPin,
+     this.CurrentType,
+     this.CurrentUsername,
+     this.documentId,
+});
+
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen>{
+
+  final FocusNode _NameFocusNode = FocusNode();
+  final FocusNode _PinFocusNode = FocusNode();
+  final FocusNode _TypeFocusNode = FocusNode();
+  final FocusNode _UserNameFocusNode = FocusNode();
+
+  bool _isDeleting = false;
+
+  @override
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: (){
+        _NameFocusNode.unfocus();
+        _PinFocusNode.unfocus();
+        _UserNameFocusNode.unfocus();
+        _TypeFocusNode.unfocus();
+      },
+     child: Scaffold(
+       backgroundColor: Colors.grey[200],
+       appBar: AppBar(
+         flexibleSpace: Container(
+           decoration: BoxDecoration(
+               gradient: LinearGradient(begin: Alignment.topRight,end: Alignment.bottomLeft,
+                   colors:[Colors.lightBlue,Colors.blue[900]] )
+           ),
+         ),
+
+         title: Text('Pins'),
+         centerTitle: true,
+         elevation: 0,
+         actions: [
+           _isDeleting ? Padding(
+             padding: const EdgeInsets.only(
+               top: 10.0,
+               bottom: 10.0,
+               right: 16.0
+             ),
+             child: CircularProgressIndicator(
+               valueColor: AlwaysStoppedAnimation<Color>(
+                 Colors.redAccent,
+               ),
+               strokeWidth: 3,
+             ),
+           )
+               :IconButton(
+             icon: Icon(
+               Icons.delete,
+               color: Colors.redAccent,
+               size: 32,
+             ),
+             onPressed: () async {
+               setState(() {
+                 _isDeleting = true;
+               });
+
+               await Pin.deletePin(
+                 docId: widget.documentId,
+               );
+
+               setState(() {
+                 _isDeleting = false;
+               });
+
+               Navigator.of(context).pop();
+               CoolAlert.show(
+                 context: context,
+                 type: CoolAlertType.success,
+                 text: "Pin delete was successful!",
+               );
+             },
+           ),
+         ],
+       ),
+       body: SafeArea(
+         child: Align(
+           alignment: Alignment.center,
+         child: Padding(
+           padding: const EdgeInsets.only(
+             left: 10.0,
+             right: 10.0,
+             bottom: 0.0,
+             top: 100.0
+           ),
+           child: EditPinForm(
+             documentId: widget.documentId,
+             nameFocusNode: _NameFocusNode,
+             pinFocusNode: _PinFocusNode,
+             userNameFocusNode: _UserNameFocusNode,
+             typeFocusNode: _TypeFocusNode,
+
+             CurrentName: widget.CurrentName,
+             CurrentPin: widget.CurrentPin,
+             CurrentUserName: widget.CurrentUsername,
+             CurrentType: widget.CurrentType,
+
+           ),
+         ),
+       ),
+       ),
+     ),
+    );
+  }
+}
---
+++ flutter_with_db_sample/lib/widgets/edit_pin_form.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:cool_alert/cool_alert.dart';
+import 'package:flutter_with_db_sample/models/pin.dart';
+
+
+class EditPinForm extends StatefulWidget {
+  final FocusNode nameFocusNode;
+  final FocusNode pinFocusNode;
+  final FocusNode userNameFocusNode;
+  final FocusNode typeFocusNode;
+
+  final String CurrentName;
+  final String CurrentPin;
+  final String CurrentUserName;
+  final String CurrentType;
+  final String documentId;
+
+  const EditPinForm({
+     this.nameFocusNode,
+     this.pinFocusNode,
+     this.userNameFocusNode,
+     this.typeFocusNode,
+
+     this.CurrentName,
+     this.CurrentPin,
+     this.CurrentUserName,
+     this.CurrentType,
+     this.documentId,
+  });
+
+  @override
+  _EditPinFormState createState() => _EditPinFormState();
+}
+
+class _EditPinFormState extends State<EditPinForm> {
+  final _editPinFormKey = GlobalKey<FormState>();
+
+
+
+  bool _isProcessing = false;
+
+   TextEditingController _nameController;
+   TextEditingController _pinController;
+   TextEditingController _usernameController;
+   TextEditingController _typeController;
+
+  bool _isVisible = true;
+
+  @override
+  void initState() {
+    _nameController = TextEditingController(
+      text: widget.CurrentName,
+    );
+
+    _pinController = TextEditingController(
+      text: widget.CurrentPin,
+    );
+
+    _usernameController = TextEditingController(
+      text: widget.CurrentUserName,
+    );
+
+    _typeController = TextEditingController(
+      text: widget.CurrentType,
+    );
+    super.initState();
+
+
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+    key: _editPinFormKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 15.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0,0.0,20.0,0.0),
+                  child: TextField(decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20.0),
+                        labelText: 'Name',
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.grey[700]
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.indigo[900]),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.lightBlue),
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    controller: _nameController,
+                    focusNode: widget.nameFocusNode, cursorColor: Colors.lightBlue,
+                  ),
+                ),
+      Visibility(
+            visible: ((_typeController.text == "1"? true:false)),
+            child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0,20.0,20.0,0.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20.0),
+                        labelText: 'User Name',
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.grey[700]
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.indigo[900]),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.lightBlue),
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    controller: _usernameController,
+                    focusNode: widget.userNameFocusNode,
+                  ),
+                  ),
+),
+                SizedBox(height: 15.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0,0.0,20.0,0.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20.0),
+                        labelText: 'PIN',
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.grey[700]
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.indigo[900]),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2,color: Colors.lightBlue),
+                            borderRadius: BorderRadius.circular(10)
+                        )
+                    ),
+                    controller: _pinController,
+                    focusNode: widget.pinFocusNode,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _isProcessing
+              ? Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                Colors.lightBlue[700],
+              ),
+            ),
+          )
+              : Container(
+            width: double.maxFinite,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Colors.lightBlue[700],
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              onPressed: () async {
+                widget.nameFocusNode.unfocus();
+                widget.pinFocusNode.unfocus();
+
+                  setState(() {
+                    _isProcessing = true;
+                  });
+
+                  await Pin.updatePins(
+                    docId: widget.documentId,
+                    name: _nameController.text,
+                    pin: _pinController.text,
+                    username: _usernameController.text,
+                  );
+
+                  setState(() {
+                    _isProcessing = false;
+                  });
+                  Navigator.of(context).pop();
+                CoolAlert.show(
+                  context: context,
+                  type: CoolAlertType.success,
+                  text: "Pin Update was successful!",
+                );
+                },
+
+
+              child: Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Text(
+                  'UPDATE PIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+}
+}
